463086be4a9dc371e19a64958884443e5ae892cf
erick.workspace <erick.workspace@hotmail.com>
2017-07-24 20:34:41 -0300

    tests passing with hackerrank

--- algorithms/1 - time-conversion/erickwendel/time-conversion_test.go
+++ algorithms/1 - time-conversion/erickwendel/time-conversion_test.go
@@ -16,6 +16,7 @@ func TestNotMidnightFormat(t *testing.T) {
   assert.Equal(result, "04:05:45AM")
   
 }
+
 func TestFormat24hoursClockMidnight (t *testing.T) {
   assert := assert.New(t)
   result := To24HoursClock("12:05:45AM")
@@ -29,8 +30,50 @@ func TestFormat24hoursClock (t *testing.T) {
   assert.Equal(result, "19:05:45")
 }
 
+
 func TestFormat24hoursClockWith12PM (t *testing.T) {
   assert := assert.New(t)
   result := To24HoursClock("12:05:45PM")
   assert.Equal(result, "12:05:45")
+}
+func TestErrorWithoutSufix (t *testing.T) {
+  assert := assert.New(t)
+  result := To24HoursClock("12:05:45")
+  assert.Equal(result, "")
+
+}
+
+func TestErrorWithEmptyString (t *testing.T) {
+  assert := assert.New(t)
+  result := To24HoursClock("")
+  assert.Equal(result, "")
+
+}
+func TestErrorWithInvalidString (t *testing.T) {
+  assert := assert.New(t)
+  result := To24HoursClock("12PM")
+  assert.Equal(result, "")
+}
+
+func TestErrorWithInvalidStringText (t *testing.T) {
+  assert := assert.New(t)
+  result := To24HoursClock("12a:05:45AM")
+  assert.Equal(result, "")
+}
+func TestErrorWithInvalidStringText_2 (t *testing.T) {
+  assert := assert.New(t)
+  result := To24HoursClock("12a:05a:45AM")
+  assert.Equal(result, "")
+}
+
+func TestErrorWithInvalidStringText_3 (t *testing.T) {
+  assert := assert.New(t)
+  result := To24HoursClock("12a:05a:45aAM")
+  assert.Equal(result, "")
+}
+
+func TestFormat24hoursClockWith7Hors (t *testing.T) {
+  assert := assert.New(t)
+  result := To24HoursClock("07:05:45AM")
+  assert.Equal(result, "07:05:45")
 }
